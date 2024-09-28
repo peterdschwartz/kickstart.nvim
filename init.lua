8f7a3d8a723cd9d7e1caaeb7cd52213a59d11e88
Peter Schwartz <peterdschwartz83@gmail.com>
2024-09-28 12:51:28 -0400

    Several new plugins and setting up the 'after' directory for ftplugins

--- init.lua
+++ init.lua
@@ -785,6 +785,7 @@ require('lazy').setup({
         -- Conform can also run multiple formatters sequentially
         python = { 'isort', 'black' },
         --
+        fortran = { 'fprettify' },
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
       },
@@ -937,6 +938,7 @@ require('lazy').setup({
         send_line = '++',
         visual_send = '+',
         send_motion = '+',
+        send_file = '+F',
       },
       config = {
         -- This defines how the repl is opened. Here, we set the REPL window
@@ -1052,13 +1054,14 @@ require('lazy').setup({
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
-  -- require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
   require 'custom.plugins.oil',
+  require 'custom.plugins.harpoon',
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -102,4 +102,19 @@ return {
       },
     }
   end,
+  -- Configuration for the python debugger
+  -- - configures debugpy for us
+  -- - uses the debugpy installation from mason
+  {
+    'mfussenegger/nvim-dap-python',
+    dependencies = 'mfussenegger/nvim-dap',
+    config = function()
+      -- fix: E5108: Error executing lua .../Local/nvim-data/lazy/nvim-dap-ui/lua/dapui/controls.lua:14: attempt to index local 'element' (a nil value)
+      -- see: https://github.com/rcarriga/nvim-dap-ui/issues/279#issuecomment-1596258077
+      -- require('dapui').setup()
+      -- uses the debugypy installation by mason
+      local debugpyPythonPath = require('mason-registry').get_package('debugpy'):get_install_path() .. '/venv/bin/python3'
+      require('dap-python').setup '/usr/bin/python3' ---@diagnostic disable-line: missing-fields
+    end,
+  },
 }
